26d8edd1a079658011d25ed84fd7d1a4e9aa4b27
Agustin Ruiz <agustinomarruiz@gmail.com>
2022-04-19 15:27:20 -0300

    Peliculas proyect - Details Screen

--- peliculas/peliculas/lib/screens/details_screen.dart
+++ peliculas/peliculas/lib/screens/details_screen.dart
@@ -1,15 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //TODO: Cambiar luego por una instancia de movie
+    // Recibo el parametro que se manda desde el movie slider y el card swiper
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
-        ),
+      body: CustomScrollView(
+        slivers: [ // los sliver son widgets que tienen comportamiento pre-programado cuando se hace scroll
+          const _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const _PosterAndTitle(),
+              const _Overview(),
+              const _Overview(),
+              const _Overview(),
+              const CastingCards(),
+            ]),
+          ),
+        ],
       ),
     );  
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          padding: const EdgeInsets.only(bottom: 5),
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+
+          const SizedBox(width: 20,),
+          
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+
+              Row(
+                children: [
+                  const Icon(Icons.star_outlined, size: 15, color: Colors.grey,),
+                  const SizedBox(width: 5,),
+                  Text('movie.voteAverage', style: textTheme.caption,)
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Voluptate adipisicing dolor labore in pariatur reprehenderit sit. Veniam minim cupidatat elit ea. Magna incididunt ex officia deserunt Lorem sit nostrud cillum id. Voluptate adipisicing dolor labore in pariatur reprehenderit sit. Veniam minim cupidatat elit ea. Magna incididunt ex officia deserunt Lorem sit nostrud cillum id. Voluptate adipisicing dolor labore in pariatur reprehenderit sit. Veniam minim cupidatat elit ea. Magna incididunt ex officia deserunt Lorem sit nostrud cillum id.',
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.subtitle1,
+      ),
+      
+    );
   }
 }
